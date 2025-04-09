3a5d19d4143d523a6a9c195be90b039751a2cb80
Improve <58224105+Gadnio@users.noreply.github.com>
2025-04-09 22:00:50 +0300

    Update db.php

--- classes/db.php
+++ classes/db.php
@@ -1,14 +1,16 @@
 <?php
 // db.php
-$host = 'localhost';
-$db = 'aether';
-$user = 'aether';
-$pass = 'cf14c097a5df9';
+
+$host = getenv('DB_HOST') ?: 'monorail.proxy.rlwy.net';
+$port = getenv('DB_PORT') ?: 11565;
+$db   = getenv('DB_NAME') ?: 'railway';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: 'hWkEMFJXMZevZqSjbLzOAqufElpcQFOq';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Error: " . $e->getMessage());
+    die("Database connection error: " . $e->getMessage());
 }
 ?>
